98ba6e2e4980205798016a2854f509ee9155b080
reb0und <me@rebound.sh>
2025-07-23 09:34:24 -0700

    feat: add hyprland to flake inputs

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # hyprland.url = "github:hyperwm/Hyprland";
+    hyprland.url = "github:hyprwm/hyprland";
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
