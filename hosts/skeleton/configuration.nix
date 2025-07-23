d35888f18d480bf40b89a02015fcdaef70969bd7
reb0und <me@rebound.sh>
2025-07-23 15:25:09 -0700

    feat: enable desktop portals and jack audio

--- hosts/skeleton/configuration.nix
+++ hosts/skeleton/configuration.nix
@@ -74,7 +74,7 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
@@ -132,6 +132,9 @@
     xwayland.enable = true;
     package = inputs.hyprland.packages."${pkgs.system}".hyprland;
   };
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
   environment.sessionVariables = {
     NIXOS_OZONE_WL = "1";
