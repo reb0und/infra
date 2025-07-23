73d112d001ad31a22fbffe004e5d84516ffb3063
reb0und <me@rebound.sh>
2025-07-23 12:45:33 -0700

    feat: link mako conf

--- modules/desktop/mako.nix
+++ modules/desktop/mako.nix
@@ -1,7 +1,7 @@
 { pkgs, config, home, ... }:
 
 {
-	programs.mako.enable = true;
+	services.mako.enable = true;
 
-	# home.file".config/mako".source = config.lib.file.mkOutOfStoreSymlink "/home/dominic/config/mako";
+	home.file".config/mako".source = config.lib.file.mkOutOfStoreSymlink "/home/dominic/config/mako";
 }
