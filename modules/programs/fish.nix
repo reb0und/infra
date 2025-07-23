f84814f4f4d970006f31c10d5adf5b94552e8361
reb0und <me@rebound.sh>
2025-07-23 02:47:21 -0700

    fix: config.fish path

--- modules/programs/fish.nix
+++ modules/programs/fish.nix
@@ -3,5 +3,5 @@
 {
 	home.packages = [ pkgs.fish ];
 	
-	home.file.".config/fish/config.fish".source = config.lib.file.mkOutOfStoreSymlink "/home/dominc/dotfiles/fish/config.fish";
+	home.file.".config/fish/config.fish".source = config.lib.file.mkOutOfStoreSymlink "/home/dominic/dotfiles/fish/config.fish";
 }
