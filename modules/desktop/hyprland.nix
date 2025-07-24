99e1ab4b619c4ff9d96741d44610de9ef7c40ca5
reb0und <me@rebound.sh>
2025-07-23 18:12:24 -0700

    fix: mv hyprland symlink to conf file

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -1,5 +1,5 @@
 { pkgs, config, home, ... }:
 
 {
-	home.file.".config/hypr".source = config.lib.file.mkOutOfStoreSymlink "/home/dominic/dotfiles/hypr";
+	home.file.".config/hypr/hyprland.conf".source = config.lib.file.mkOutOfStoreSymlink "/home/dominic/dotfiles/hypr/hyprland.conf";
 }
