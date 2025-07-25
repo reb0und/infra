1d9494302cb5a90828530576c30038e8e6e9f9c6
reb0und <me@rebound.sh>
2025-07-25 09:34:20 -0700

    feat: spicetify

--- modules/desktop/spicetify.nix
+++ modules/desktop/spicetify.nix
@@ -1,7 +1,17 @@
 { pkgs, config, home, inputs, ... }:
 
+let
+	spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
+in
 {
-	imports = [ inputs.spicetify-nix.homeManagerModules.default ];
+	imports = [ inputs.spicetify-nix.homeManagerModules.spicetify ];
 
-	programs.spicetify.enable = true;
+	programs.spicetify = {
+		enable = true;
+		enabledExtensions = with spicePkgs.extensions; [
+			adblockify
+			hidePodcasts
+			shuffle
+		];
+	};
 }
