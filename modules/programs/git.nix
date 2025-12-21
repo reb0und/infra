f4db18ceb68778cd549f5a64db2ad033be408eb5
reb0und <me@rebound.sh>
2025-12-21 04:59:52 -0500

    fix: git cfg

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -4,8 +4,10 @@
 	programs.git = {
 		enable = true;
 		settings = {
-			name = "reb0und";
-			email = "me@rebound.sh";
+			user = {
+				name = "reb0und";
+				email = "me@rebound.sh";
+			};
 			core.editor = "nvim";
 			init.defaultBranch = "main";
 		};
