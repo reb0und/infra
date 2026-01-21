074ebdfc0633e9878eb30258226bfb7fd0da612c
reb0und <me@rebound.sh>
2026-01-21 00:16:38 -0500

    fix: vm usage

---
+++ modules/vms/hajin/flake.nix
@@ -0,0 +1,25 @@
+{
+	description = "";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+		nixos-generators = {
+			url = "github:nix-community/nixos-generators";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+
+	outputs = { self, nixpkgs, nixos-generators, }:
+		let
+			system = "x86_64-linux";
+		in {
+			image = nixos-generators.nixosGenerate {
+				system = system;
+				modules = [ ./configuration.nix ];
+				format = "qcow2";
+				diskSize = 30 * 1024;
+				memorySize = 4096;
+				cores = 4;
+			};
+		};
+}
