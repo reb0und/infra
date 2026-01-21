504cf9cf8f5048a4642967848723fe5a88a8f30f
reb0und <me@rebound.sh>
2026-01-20 23:38:45 -0500

    feat: hajin vm cfg

--- modules/vms/hajin/configuration.nix
+++ modules/vms/hajin/configuration.nix
@@ -1,5 +1,22 @@
 { config, pkgs, ... }:
 
 {
+	boot.loader = {
+		systemd.enable = true;
+		efi.canTouchEfiVariables = true;
+	};
 
+	users.users.hajin = {
+		isNormalUser = true;
+		extraGroups = [ "wheel" ];
+		initialPassword = "hi";
+	};
+
+	environment.systemPackages = with pkgs; [
+		fish
+		git
+		vim
+	];
+
+	system.stateVersion = "25.11";
 }
