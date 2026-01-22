dfa50fd51719ec0fd73f605c132129709fc6a2cc
dominic <me@rebound.sh>
2026-01-22 01:08:37 -0500

    feat: test vm

---
+++ modules/vms/test/configuration.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }:
+
+{
+	boot.loader = {
+		grub.enable = true;
+		grub.version = 2;
+		grub.device = "/dev/vda";
+	};
+
+	users.users.dominic = {
+		isNormalUser = true;
+		extraGroups = [ "wheel" "docker" ];
+		initialPassword = "hi";
+		openssh.authorizedKeys.keys = [
+			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ2vY3uVDZbTm8NYFPXSXswwxaKZ/BMc4MaIzrKyG1u1 dominic@skeleton"
+		];
+	};
+
+	environment.systemPackages = with pkgs; [
+		fish
+		git
+		vim
+	];
+
+	virtualisation = {
+		diskSize = 30 * 1024;
+		docker.enable = true;
+	};
+
+	services = {
+		openssh.enable = true;
+		tailscale.enable = true;
+	};
+
+	system.stateVersion = "25.11";
+}
---
+++ modules/vms/test/flake.nix
@@ -0,0 +1,22 @@
+{
+	description = "";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+		nixos-generators = {
+			url = "github:nix-community/nixos-generators";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+
+	outputs = { self, nixpkgs, nixos-generators, }:
+		let
+			system = "x86_64-linux";
+		in {
+			image = nixos-generators.nixosGenerate {
+				system = system;
+				modules = [ ./configuration.nix ];
+				format = "qcow";
+			};
+		};
+}
