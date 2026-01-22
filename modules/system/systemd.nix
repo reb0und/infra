808afb711e9882816287cf7193dc027506c71582
dominic <me@rebound.sh>
2026-01-21 22:40:44 -0500

    fix: typos in sysstem service def

--- modules/system/systemd.nix
+++ modules/system/systemd.nix
@@ -4,21 +4,20 @@
 	systemd.user.services.hajin-vm = {
 		description = "Hajin VM"; enable = true;
 		after = [ "network.target" ];
+		path = [ pkgs.qemu ];
 		serviceConfig = {
 			ExecStart = ''
 				qemu-system-x86_64 \
 				-nographic \
-				-kda /home/dominic/images/hajin/hajin.qcow2 \
+				-hda /home/dominic/images/hajin/hajin.qcow2 \
 				-m 4096 \
-				-ssmp 4 \
+				-smp 4 \
 				-enable-kvm
 			'';
 			Restart = "always";
 			KillMode = "process";
 			TimeoutStopSec = 30;
 			StandardInput = "null";
-			User = "dominic";
-			Group = "dominic";
 		};
 		wantedBy = [ "default.target" ];
 	};
