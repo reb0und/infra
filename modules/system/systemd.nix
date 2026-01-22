7a148e63ca8809d499e10290607a7d78179a7a77
dominic <me@rebound.sh>
2026-01-22 01:28:31 -0500

    feat: test vm systemd service

--- modules/system/systemd.nix
+++ modules/system/systemd.nix
@@ -21,4 +21,27 @@
 		};
 		wantedBy = [ "default.target" ];
 	};
+
+	systemd.user.services.test = {
+		description = "Test VM"; enable = true;
+		after = [ "network.target" ];
+		path = [ pkgs.qemu ];
+		serviceConfig = {
+			ExecStart = ''
+				${pkgs.qemu}/bin/qemu-system-x86_64 \
+				-nographic \
+				-hda /home/dominic/images/test/test.qcow2 \
+				-m 512 \
+				-smp 1 \
+				-netdev user,id=net0,hostfwd=tcp::11222-:22 \
+				-device virtio-net-pci,netdev=net0 \
+				-enable-kvm
+			'';
+			Restart = "always";
+			KillMode = "process";
+			TimeoutStopSec = 30;
+			StandardInput = "null";
+		};
+		wantedBy = [ "default.target" ];
+	};
 }
