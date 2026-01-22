a00d4831212c02d3205b1ceec94cf56470073f5d
reb0und <me@rebound.sh>
2026-01-22 17:47:09 -0500

    refactor: hajin vm systemd service

--- modules/system/systemd.nix
+++ modules/system/systemd.nix
@@ -1,27 +1,6 @@
 { config, pkgs, ... }:
 
 {
-	systemd.user.services.hajin-vm = {
-		description = "Hajin VM"; enable = true;
-		after = [ "network.target" ];
-		path = [ pkgs.qemu ];
-		serviceConfig = {
-			ExecStart = ''
-				${pkgs.qemu}/bin//qemu-system-x86_64 \
-				-nographic \
-				-hda /home/dominic/images/hajin/hajin.qcow2 \
-				-m 4096 \
-				-smp 4 \
-				-enable-kvm
-			'';
-			Restart = "always";
-			KillMode = "process";
-			TimeoutStopSec = 30;
-			StandardInput = "null";
-		};
-		wantedBy = [ "default.target" ];
-	};
-
 	systemd.user.services.test = {
 		description = "Test VM"; enable = true;
 		after = [ "network.target" ];
@@ -34,6 +13,29 @@
 				-m 512 \
 				-smp 1 \
 				-netdev user,id=net0,hostfwd=tcp::11222-:22 \
+				-device virtio-net-pci,netdev=net0 \
+				-enable-kvm
+			'';
+			Restart = "always";
+			KillMode = "process";
+			TimeoutStopSec = 30;
+			StandardInput = "null";
+		};
+		wantedBy = [ "default.target" ];
+	};
+
+	systemd.user.services.hajin-vm = {
+		description = "Hajin VM"; enable = true;
+		after = [ "network.target" ];
+		path = [ pkgs.qemu ];
+		serviceConfig = {
+			ExecStart = ''
+				${pkgs.qemu}/bin/qemu-system-x86_64 \
+				-nographic \
+				-hda /home/dominic/images/hajin/hajin.qcow2 \
+				-m 512 \
+				-smp 1 \
+				-netdev user,id=net0,hostfwd=tcp::11223-:22 \
 				-device virtio-net-pci,netdev=net0 \
 				-enable-kvm
 			'';
