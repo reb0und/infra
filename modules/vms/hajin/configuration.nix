9784973b67eb3375e81e4078db71f62a1ef7ebcb
reb0und <me@rebound.sh>
2026-01-22 17:49:25 -0500

    feat: hajin system service & ssh key

--- modules/vms/hajin/configuration.nix
+++ modules/vms/hajin/configuration.nix
@@ -10,6 +10,9 @@
 		isNormalUser = true;
 		extraGroups = [ "wheel" "docker" ];
 		initialPassword = "hi";
+		openssh.authorizedKeys.keys = [
+			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID0fQldW2bHZphOxdLX+qQgLmvGHX3yTx/JbpiW7RUc0 zero734kr@gmail.com"
+		];
 	};
 
 	environment.systemPackages = with pkgs; [
