38a469e7aa79c114ceb9acfe824de5ff90b1e10c
reb0und <me@rebound.sh>
2026-01-29 18:01:13 -0500

    chore: add memory to hajin vm

--- modules/system/systemd.nix
+++ modules/system/systemd.nix
@@ -34,7 +34,7 @@
 				-nographic \
 				-hda /home/dominic/images/hajin/hajin.qcow2 \
 				-m 4096 \
-				-smp 4 \
+				-smp 8 \
 				-netdev user,id=net0,hostfwd=tcp::11223-:22 \
 				-device virtio-net-pci,netdev=net0 \
 				-enable-kvm
