e717bb12eb1a25e674c0483e7f7e90d4ec812c44
reb0und <me@rebound.sh>
2026-02-21 02:04:42 -0500

    fix: wg client conf

--- hosts/sparrow/wg.nix
+++ hosts/sparrow/wg.nix
@@ -2,7 +2,7 @@
 
 {
   environment.systemPackages = with pkgs; [
-    wireguard-tools;
+    wireguard-tools
   ];
 
   networking.firewall = {
@@ -13,18 +13,18 @@
     wg0 = {
       ips = [ "10.100.0.2/24" ];
       listenPort = 51820;
+
+      # @TODO: Encrypt using SOPS
+      privateKeyFile = "/home/dominic/keys/wireguard/private"; 
+
+      peers = [
+        {
+          publicKey = "z7tCgSZWI0a5LkkLQrqo5YAV/q2chYOz16uwGxQk0WA=";
+          allowedIPs = [ "10.100.0.2/24" ];
+          endpoint = "54.163.192.2:8172";
+          persistentKeepAlive = 25;
+        }
+      ];
     };
-
-    # @TODO: Encrypt using SOPS
-    privateKeyFile = "/home/dominic/keys/wireguard/private"; 
-
-    peers = [
-      {
-        publicKey = "";
-        allowedIPs = [ "0.0.0.0/0" ];
-        endpoint = "ip:51820";
-        persistentKeepAlive = 25;
-      }
-    ];
   };
 }
