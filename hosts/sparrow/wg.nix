9a01497e864c9b5ce3305d95bcbfb52e6538dfb0
reb0und <me@rebound.sh>
2026-02-21 02:55:25 -0500

    fix: private key path, allowed ip range, keep alive typo

--- hosts/sparrow/wg.nix
+++ hosts/sparrow/wg.nix
@@ -11,18 +11,18 @@
 
   networking.wireguard.interfaces = {
     wg0 = {
-      ips = [ "10.100.0.2/24" ];
+      ips = [ "10.100.0.2/32" ];
       listenPort = 51820;
 
       # @TODO: Encrypt using SOPS
-      privateKeyFile = "/home/dominic/keys/wireguard/private"; 
+      privateKeyFile = "/home/dominic/keys/wg/private"; 
 
       peers = [
         {
           publicKey = "z7tCgSZWI0a5LkkLQrqo5YAV/q2chYOz16uwGxQk0WA=";
-          allowedIPs = [ "10.100.0.2/24" ];
+          allowedIPs = [ "10.100.0.2/32" ];
           endpoint = "54.163.192.2:8172";
-          persistentKeepAlive = 25;
+          persistentKeepalive = 25;
         }
       ];
     };
