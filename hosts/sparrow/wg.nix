7023c05d940181b67cee8605299c046fe70f9986
reb0und <me@rebound.sh>
2026-02-21 01:01:43 -0500

    fix: add wg to pkgs

--- hosts/sparrow/wg.nix
+++ hosts/sparrow/wg.nix
@@ -1,7 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  programs.wireguard-tools.enable = true;
+  environment.systemPackages = with pkgs; [
+    wireguard-tools;
+  ];
 
   networking.firewall = {
     allowedUDPPorts = [ 51820 ];
@@ -14,7 +16,7 @@
     };
 
     # @TODO: Encrypt using SOPS
-    privateKeyFile = "~/keys/wireguard/private"; 
+    privateKeyFile = "/home/dominic/keys/wireguard/private"; 
 
     peers = [
       {
