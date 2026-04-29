d39da1b49bda1c4e368ee5c87213edd8a9825ee9
reb0und <me@rebound.sh>
2026-04-28 23:50:19 -0400

    feat: zulip term

--- modules/desktop/zulip.nix
+++ modules/desktop/zulip.nix
@@ -1,5 +1,12 @@
-{ pkgs, home, ... }: {
+{ pkgs, pkgs-unstable, home, inputs, ... }: 
+let
+	pkgs-unstable = import inputs.nixpkgs-unstable {
+		system = pkgs.stdenv.hostPlatform.system;
+	};
+in
+{
 	home.packages = [
 		pkgs.zulip
+		pkgs-unstable.zulip-term
 	];
 }
