cb82ae4651ee07788dcc14c21de529fe52b594fb
reb0und <me@rebound.sh>
2026-04-29 00:16:50 -0400

    feat: zulip-term cfg

--- modules/desktop/zulip.nix
+++ modules/desktop/zulip.nix
@@ -1,4 +1,5 @@
-{ pkgs, pkgs-unstable, home, inputs, ... }: 
+# @TODO: Add sops input for ZUlip secrets
+{ pkgs, pkgs-unstable, home, inputs, config, ... }: 
 let
 	pkgs-unstable = import inputs.nixpkgs-unstable {
 		system = pkgs.stdenv.hostPlatform.system;
@@ -9,4 +10,7 @@ in
 		pkgs.zulip
 		pkgs-unstable.zulip-term
 	];
+
+	# @TODO: Change this to secret contents
+	home.file.".config/zulip-term/.zuliprc".source = config.lib.file.mkOutOfStoreSymlink "/home/dominic/dotfiles/zulip-term/.zuliprc";
 }
