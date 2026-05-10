ca7426b7a327999fb988b0f6505fee7d32fdec78
reb0und <me@rebound.sh>
2026-05-10 10:27:19 -0400

    fix: sops default file

--- modules/system/sops.nix
+++ modules/system/sops.nix
@@ -4,6 +4,6 @@ let
 	sops = sops-nix.nixosModules.sops; 
 in 
 {
-	sops.defaultSopsFile = "../../.sops.yaml";
+	sops.defaultSopsFile = "../../secrets/secret.enc.yaml";
 	sops.age.sshKeyPaths = [ "~/.ssh/id_ed25519.pub" ];
 }
