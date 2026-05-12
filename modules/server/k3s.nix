f028ac58d5515008cf68fea607e3eef965a8f76d
reb0und <me@rebound.sh>
2026-05-12 11:28:22 -0400

    chore: k3s resolve conf cfg

--- modules/server/k3s.nix
+++ modules/server/k3s.nix
@@ -1,17 +1,23 @@
 { config, pkgs, ... }: 
 
 {
+	environment.etc."k3s-resolve.conf".text = ''
+		nameserver 1.1.1.1
+		nameserver 8.8.8.8
+	'';
+
 	services.k3s = {
 		enable = true;
   	  	role = "server";
   	  	clusterInit = true;
   	  	extraFlags = [
+			"--resolve-conf=k3s-resolve.conf"
 			"--node-ip=155.246.36.10"
   	  	  	"--disable-network-policy"
   	  	  	"--flannel-backend=none"
 			"--disable-kube-proxy"
 			"--disable=traefik"
-			"--disable=servicelib"
+			"--disable=servicelb"
 			"--disable=metrics-server"
   	  	];
   	};
