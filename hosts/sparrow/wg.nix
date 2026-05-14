fa2140d45700b0a4ea7d394504a91cdf9b4ba0ad
reb0und <me@rebound.sh>
2026-05-14 10:33:44 -0400

    fix: wg conf

--- hosts/sparrow/wg.nix
+++ hosts/sparrow/wg.nix
@@ -17,10 +17,6 @@
       # @TODO: Encrypt using SOPS
       privateKeyFile = "/home/dominic/keys/wg/private"; 
 
-      postUp = "ip route add default dev wg0 table 200; ip rule add from 10.100.0.2/32 table 200";
-
-      postDown = "ip rule del from 10.100.0.2/32 table 200; ip route del default dev wg0 table 200";
-
       peers = [
         {
           publicKey = "z7tCgSZWI0a5LkkLQrqo5YAV/q2chYOz16uwGxQk0WA=";
