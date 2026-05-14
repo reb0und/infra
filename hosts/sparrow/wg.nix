d2a90d53a2353e7fb5649466094a3563fe52de9c
dominic <me@rebound.sh>
2026-05-14 07:57:07 -0400

    fix: wg postup and postdown options

--- hosts/sparrow/wg.nix
+++ hosts/sparrow/wg.nix
@@ -17,9 +17,9 @@
       # @TODO: Encrypt using SOPS
       privateKeyFile = "/home/dominic/keys/wg/private"; 
 
-      postUp = "ip route add default dev wg0 table 200; ip rule add from 10.100.0.2/32 table 200";
+      postSetup = "ip route add default dev wg0 table 200; ip rule add from 10.100.0.2/32 table 200";
 
-      postDown = "ip rule del from 10.100.0.2/32 table 200; ip route del default dev wg0 table 200";
+      postShutdown = "ip rule del from 10.100.0.2/32 table 200; ip route del default dev wg0 table 200";
 
       peers = [
         {
