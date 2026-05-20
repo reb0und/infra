20510574986ba45fa0bfd8d73cb2dafc9b06f54e
reb0und <me@rebound.sh>
2026-05-20 15:52:58 -0400

    feat: autoupgrade system

--- hosts/skeleton/configuration.nix
+++ hosts/skeleton/configuration.nix
@@ -193,6 +193,10 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+  };
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
