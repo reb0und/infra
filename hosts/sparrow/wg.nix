bdd5b6fd41c44e467c3a2e4825d5074d478d835e
reb0und <me@rebound.sh>
2026-07-08 13:12:27 -0400

    feat: add protonvpn wg server

--- hosts/sparrow/wg.nix
+++ hosts/sparrow/wg.nix
@@ -6,13 +6,21 @@
   ];
 
   networking.firewall = {
-    allowedUDPPorts = [ 51820 ];
+    allowedUDPPorts = [ 51820 51821 ];
   };
 
   systemd.services.wg0-routing = {
     wantedBy = [ "multi-user.target" ];
-    after = [ "wireguard-wg0.service" "tailscaled.service" "NetworkManager.service" ];
-    requires = [ "wireguard-wg0.service" ];
+    after = [ 
+      "wireguard-wg0.service"
+      "wireguard-wg-proton-vpn.service"
+      "tailscaled.service"
+      "NetworkManager.service"
+    ];
+    requires = [ 
+      "wireguard-wg0.service"
+      "wireguard-wg-proton-vpn.service"
+    ];
   
     path = [ pkgs.iproute2 ];
   
@@ -25,16 +33,24 @@
       set -x
   
       ip route replace default dev wg0 table 51820
+      ip route replace default dev wg-proton-vpn table 51821
   
+      ip rule del priority 99 2>/dev/null || true
+      ip rule add priority 99 from 10.100.0.2/32 lookup 51820
+
       ip rule del priority 98 2>/dev/null || true
-      ip rule add priority 98 from 10.100.0.2/32 lookup 51820
+      ip rule add priority 98 from 10.8.0.2/32 lookup 51821
   
       ip route flush cache || true
     '';
   
     preStop = ''
-      ip rule del priority 98 2>/dev/null || true
+      ip rule del priority 99 2>/dev/null || true
       ip route flush table 51820 || true
+
+      ip rule del priority 98 2>/dev/null || true
+      ip route flush table 51821 || true
+
       ip route flush cache || true
     '';
   };
@@ -52,8 +68,27 @@
       peers = [
         {
           publicKey = "z7tCgSZWI0a5LkkLQrqo5YAV/q2chYOz16uwGxQk0WA=";
-          allowedIPs = [ "0.0.0.0/0" ];
+          allowedIPs = [ "10.100.0.1/32" "10.200.0.0/24" ];
           endpoint = "5.161.200.224:8172";
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+
+    wg-proton-vpn = {
+      ips = [ "10.8.0.2/32" ];
+      listenPort = 51821;
+
+      # @TODO: Encrypt using SOPS
+      privateKeyFile = "/home/dominic/keys/wg/private_proton_vpn"; 
+
+      allowedIPsAsRoutes = false;
+
+      peers = [
+        {
+          publicKey = "3XROzgS72QDRKE3Z2XcnppVNFPaLFCeSRDmsCe6EGws=";
+          allowedIPs = [ "0.0.0.0/0" ];
+          endpoint = "149.34.251.138:51820";
           persistentKeepalive = 25;
         }
       ];
