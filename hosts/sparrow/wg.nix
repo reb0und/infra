f817bd75ef352538b4c136722e976856f061f64c
reb0und <me@rebound.sh>
2026-07-08 13:43:47 -0400

    fix: wg routes

--- hosts/sparrow/wg.nix
+++ hosts/sparrow/wg.nix
@@ -32,6 +32,9 @@
     script = ''
       set -x
   
+      ip route replace 10.100.1.0/32 dev wg0
+      ip route replace 10.200.0.0/32 dev wg0
+
       ip route replace default dev wg0 table 51820
       ip route replace default dev wg-proton-vpn table 51821
   
@@ -45,6 +48,9 @@
     '';
   
     preStop = ''
+      ip route del 10.100.1.0/32 dev wg0 2>/dev/null || true     
+      ip route del 10.200.0.0/32 dev wg0 2>/dev/null || true         
+
       ip rule del priority 99 2>/dev/null || true
       ip route flush table 51820 || true
 
